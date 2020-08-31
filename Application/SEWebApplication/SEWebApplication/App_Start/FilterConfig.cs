df15142db18d2657a92fa9690cc40cbccdb24acf
highpies <guanwei58@hotmail.com>
2020-08-31 23:47:47 +0800

    Created application file
    Just a shell

---
+++ Application/SEWebApplication/SEWebApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SEWebApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Application/SEWebApplication/SEWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEWebApplication.MvcApplication" Language="C#" %>
