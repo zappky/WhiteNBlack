6381c0d3bfc2d2500c6a6dffc03c50af6830b8af
highpies <guanwei58@hotmail.com>
2020-09-18 10:14:48 +0800

    Removed previous classes
    Removed previous entity classes

---
+++ Application/SEWebApplication/SEWebApplication/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEWebApplication.Models
+{
+    public class User
+    {
+        public string userName { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int age { get; set; }
+        public string profilePicture { get; set; }
+        public Boolean verified { get; set; }
+    }
+}
