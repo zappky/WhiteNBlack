27dcf8265fc5c1298c6b93b81dcb6bcf701b73bf
Heather <heather9729@gmail.com>
2020-10-16 15:13:02 +0800

    Article Update

--- Application/SmokeFreeApplication/SmokeFreeApplication/Controllers/ArticleController.cs
+++ Application/SmokeFreeApplication/SmokeFreeApplication/Controllers/ArticleController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using SmokeFreeApplication.Models;
+using PagedList;
 
 
 namespace SmokeFreeApplication.Controllers
@@ -20,11 +22,37 @@ namespace SmokeFreeApplication.Controllers
                 return RedirectToAction("SignInDoctor", "Account");
             }
 
-            return View(smokeFreeDB.Article.ToList());
+            int pageSize = 1;
+            int pageNumber = (page ?? 1);
+            return View(smokeFreeDB.Story.ToList().ToPagedList(pageNumber, pageSize));
+        }
+        public FileContentResult retrieveUserPic(string username)
+        {
+            byte[] imgByteArray = smokeFreeDB.GeneralUser.Find(username).profilePicture;
+            if (imgByteArray != null)
+            {
+                return new FileContentResult(imgByteArray, "image/jpeg");
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public ActionResult ViewArticle(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = smokeFreeDB.Story.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
         }
 
-        // Borrow code from ray!
     }
 
-    }
 }
