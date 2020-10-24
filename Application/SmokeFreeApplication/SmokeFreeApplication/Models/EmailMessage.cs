4a1fb702b9e74d1be9930ef02bf6fd31870a59d7
Zappky <unconfigured@null.spigotmc.org>
2020-10-25 02:54:35 +0800

    Email Notification feature
    -added a form to fill in for reject article/doc
    - added email notification which send a message to the specified email addrs
    if not email subject/body is rewritten, default msg is send

---
+++ Application/SmokeFreeApplication/SmokeFreeApplication/Models/EmailMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+namespace SmokeFreeApplication.Models
+{
+    public class EmailMessage
+    {
+        public string From
+        {
+            get;
+            set;
+        }
+        public string To
+        {
+            get;
+            set;
+        }
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string Body
+        {
+            get;
+            set;
+        }
+
+    }
+}
