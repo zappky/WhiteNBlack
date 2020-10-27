7b564361df6fb8b271241433da0a1a1cc55d225e
Zappky <unconfigured@null.spigotmc.org>
2020-10-28 03:23:25 +0800

    Ui updates
    -Fixed boardcast typo to broadcast in the layout
    -Added close button to reject doc/article
    -Changed the style for close/approve button
    -deleted unnesscary code in content.cs
    -added default msg sending upon approving article/doc (Not Tested)
    -Removed to and from field from reject msg form. Now uses system mail to send(my throwaway gmail acc), and send to the email of the doc he registered with

--- Application/SmokeFreeApplication/SmokeFreeApplication/Models/Content.cs
+++ Application/SmokeFreeApplication/SmokeFreeApplication/Models/Content.cs
@@ -15,34 +15,7 @@ namespace SmokeFreeApplication.Models
         public string body { get; set; }
         public DateTime postDate { get; set; }
 
-        public bool DeleteContent()
-        {
-           
-
-            return true;
-        }
 
     }
 
-
-    //Contain list of articles or story
-    //contain method to manage story/article
-    //such as delete, insert, edit, share? 
-    //Maybe not be needed as we have database which we can directly manupliate
-    //Not familar with website coding enough to be certain
-    //instantiate this class like
-    //ContentManager<Story> a =  new ContentManager<Story>();
-    /*public class ContentManager<T> where T : class
-    {
-        public List<T> contentsList { get; set; }
-
-        public ContentManager() { }
-
-        public bool DeleteContent(Content aContent)
-        {
-            return aContent.DeleteContent();
-        }
-
-
-    }*/
 }
